9f3ee7fb597ee649e45e8b53c28dba5dcd744233
Gilsabo <gilsala_88@hotmail.com>
2024-01-16 17:41:11 +0100

    Remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  declarations: [AppComponent, CartComponent, SaleComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
