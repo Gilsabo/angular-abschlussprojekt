e1a36f51c795975bf215cdee7b2f69b6e3cbc74c
Gilsabo <gilsala_88@hotmail.com>
2024-01-17 11:53:56 +0100

    Add Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { HomeComponent } from './pages/home/home.component';
 import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
-  declarations: [AppComponent, CartComponent, SaleComponent],
+  declarations: [AppComponent, CartComponent, SaleComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
