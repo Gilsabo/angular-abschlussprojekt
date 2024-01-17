6cf7c41e68c5c0f6d16e1d5c78107b89ac6d62d6
Gilsabo <gilsala_88@hotmail.com>
2024-01-17 11:52:01 +0100

    Add function to add to cart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  productList: any[] = [];
+
+  cartObj: any = {
+    CartId: 0,
+    CustId: 1,
+    ProductId: 0,
+    Quantity: 0,
+    AddedDate: '2024-01-17T10:05:53.480Z',
+  };
+  constructor(private productService: ProductService) {}
+  ngOnInit(): void {
+    this.loadAllProducts();
+  }
+
+  loadAllProducts() {
+    this.productService.getAllProducts().subscribe((result: any) => {
+      this.productList = result.data;
+    });
+  }
+  addItemToCart(productId: number) {
+    this.cartObj.ProductId = productId;
+    this.productService.addToCart(this.cartObj).subscribe((result: any) => {
+      if (result.result) {
+        alert('Product Added To Cart');
+        this.productService.cartAddedSubject.next(true);
+      }
+    });
+  }
+}
