7ff49f57963edf1e486ef769ed17a5a5e89fd6d6
Gilsabo <gilsala_88@hotmail.com>
2024-01-19 10:35:32 +0100

    Add blog componene

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SaleComponent } from './pages/sale/sale.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'products', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'sale', component: SaleComponent },
+  { path: 'blog', component: BlogComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './pages/blog/blog.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
-  declarations: [AppComponent, CartComponent, SaleComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    CartComponent,
+    SaleComponent,
+    HomeComponent,
+    BlogComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
