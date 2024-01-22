0573446fee9338e1b5d43321ffb8801fcc2761fb
Gilsabo <gilsala_88@hotmail.com>
2024-01-22 10:03:27 +0100

    Add contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './pages/blog/blog.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SaleComponent } from './pages/sale/sale.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'sale', component: SaleComponent },
   { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
