4a133e9d47a780b3549c50214be105fadf9ab04d
Gilsabo <gilsala_88@hotmail.com>
2024-01-23 16:24:13 +0100

    Remove cart from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './pages/blog/blog.component';
-import { CartComponent } from './pages/cart/cart.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SaleComponent } from './pages/sale/sale.component';
@@ -9,7 +8,6 @@ import { SaleComponent } from './pages/sale/sale.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'products', component: HomeComponent },
-  { path: 'cart', component: CartComponent },
   { path: 'sale', component: SaleComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'contact', component: ContactComponent },
