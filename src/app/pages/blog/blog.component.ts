8bc3df57a2db15e4b4da69e8784fbeadaec29e82
Gilsabo <gilsala_88@hotmail.com>
2024-01-23 16:14:48 +0100

    Add functions

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+// blog.component.ts
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Blog } from '../../Blogs';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  @Input() blogs: Blog[] = [];
 
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.blogService.getBlogs().subscribe((blogs) => (this.blogs = blogs));
+  }
+
+  deleteBlog(blog: Blog) {
+    this.blogService
+
+      .deleteBlog(blog)
+
+      .subscribe(
+        () => (this.blogs = this.blogs.filter((item) => item.id !== blog.id))
+      );
+  }
+  addBlog(blog: Blog) {
+    this.blogService
+      .createBlogService(blog)
+      .subscribe((blog) => this.blogs.push(blog));
+  }
 }
