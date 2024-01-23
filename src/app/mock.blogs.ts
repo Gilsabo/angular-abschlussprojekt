8b46e0596407152c4f86893e9f96077455d05700
Gilsabo <gilsala_88@hotmail.com>
2024-01-23 16:22:41 +0100

    Add mock data

---
+++ src/app/mock.blogs.ts
@@ -0,0 +1,19 @@
+// mock blogs
+
+import { Blog } from './Blogs';
+
+export const blogs: Blog[] = [
+  {
+    id: 1,
+    title: 'OnePlus Open',
+    content: 'Great cellphone. Super powerfull and the battery lasts 2 days.',
+    created_at: '2024-01-22',
+  },
+  {
+    id: 2,
+    title: 'Iphone15 pro',
+    content:
+      'Really fast performance. Incredible camera. The battey could be improveble',
+    created_at: '2024-01-23',
+  },
+];
